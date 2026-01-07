b596e3009e3715da1c8b518bbad58a13d8c4722c
ZDeutch <zander.deutch@menloschool.org>
2026-01-07 14:58:45 -0800

    Documented Code

--- src/Boggle.java
+++ src/Boggle.java
@@ -6,14 +6,18 @@ public class Boggle {
     public static String[] findWords(char[][] board, String[] dictionary) {
 
         ArrayList<String> goodWords = new ArrayList<String>();
-        TST tst = new TST();
 
+        // Build a TST to store all of the words in dictionary
+
+        TST tst = new TST();
         for (int i = 0; i < dictionary.length; i++) {
             tst.insert(dictionary[i]);
         }
 
+        // Use temp arrayList to manipulate the words that are found
         ArrayList<String> foundWords = new ArrayList<>();
 
+        // Create a visited array for each starting position in the board to track available moves
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 boolean[][] visited = new boolean[board.length][board[0].length];
@@ -21,6 +25,7 @@ public class Boggle {
             }
         }
 
+        // Remove duplicates - a word might be formable from multiple starting positions
         for (int i = 0; i < foundWords.size(); i++) {
             goodWords.add(foundWords.get(i));
         }
@@ -33,66 +38,44 @@ public class Boggle {
         return sol;
     }
 
-    public int numIslands(char[][] grid, boolean[][] visited, TST tst, ArrayList<String> foundWords) {
-        int count = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    IslandDFS(grid, i, j);
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
-    public static void IslandDFS(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) {
-            return;
-        }
-
-        if (grid[i][j] == '0') {
-            return;
-        }
-
-        grid[i][j] = '0';
-
-        IslandDFS(grid, i - 1, j);
-        IslandDFS(grid, i + 1, j);
-        IslandDFS(grid, i, j + 1);
-        IslandDFS(grid, i, j - 1);
-    }
+    // Depth-First Search to find words on the boggle board
+    // Uses backtracking to explore all possible paths
     public static void DFS(char[][] grid, int i, int j, String current, boolean[][] visited, TST tst, ArrayList<String> goodWords) {
+        // Base Case 1 is if any of the coordinates are out of bounds for the board
         if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) {
             return;
         }
 
-        if (grid[i][j] == '0') {
-            return;
-        }
-        if(visited[i][j]) {
+        // Base Case 2 is if the cell is already used in the word path
+        if (visited[i][j]) {
             return;
         }
 
+        // Add the current cells character to build the new word
         String newWord = current + grid[i][j];
 
-        if(!tst.hasPrefix(newWord)) {
+        // Stops if no dictionary word starts with this prefix
+        if (!tst.hasPrefix(newWord)) {
             return;
         }
 
+        // Marks current cell as visited so it doesn't repeat the letter
         visited[i][j] = true;
 
+        // If the current word is in dictionary then add to results
         if (tst.find(newWord)) {
-            goodWords.add(newWord);
+            if (!goodWords.contains(newWord)) {
+                goodWords.add(newWord);
+            }
         }
 
-        // Mark this square as visited
-
+        // Recursively explore all 4 adjacent directions
         DFS(grid, i - 1, j, newWord, visited, tst, goodWords);
         DFS(grid, i + 1, j, newWord, visited, tst, goodWords);
         DFS(grid, i, j - 1, newWord, visited, tst, goodWords);
         DFS(grid, i, j + 1, newWord, visited, tst, goodWords);
 
+        // Unmark cell so other paths can use it
         visited[i][j] = false;
     }
 }
--- src/TST.java
+++ src/TST.java
@@ -1,5 +1,3 @@
-import org.w3c.dom.Node;
-
 public class TST {
     // Root tracks the current node in the word
     NodeTST root;
@@ -11,9 +9,22 @@ public class TST {
 
     // Method to insert a string into the TST
     public void insert(String s) {
+        // Handle edge cases
+        if (s == null || s.length() == 0) {
+            return;
+        }
+
+        // Initialize root if this is first insertion
+        if (root == null) {
+            root = new NodeTST(s.charAt(0));
+        }
+
         NodeTST temp = root;
 
+
+        // Iterate each character in the word
         for (int i = 0; i < s.length(); i++) {
+            // Go left if character is smaller, right if larger
             while (s.charAt(i) != temp.getCurrent()) {
                 if (s.charAt(i) < temp.getCurrent()) {
                     if (temp.getLeft() == null) {
@@ -27,51 +38,68 @@ public class TST {
                     temp = temp.getRight();
                 }
             }
+
+            // Once the node is created, add a middle path to the next character
+            // As long as its not at the end
             if (i < s.length() - 1) {
                 if (temp.getMiddle() == null) {
-                    temp.setMiddle(s.charAt(i));
+                    temp.setMiddle(s.charAt(i + 1));
                 }
                 temp = temp.getMiddle();
             }
         }
+
+        // Mark the last node as end of word
         temp.setWord(true);
     }
 
+    // Checks if any word in the TST starts with the given prefix
     public boolean hasPrefix(String s) {
+        // Makes sure string isn't null
         if (s == null || s.length() == 0) {
             return false;
         }
+        // Returns whether it appears in the TST
         return get(root, s, 0) != null;
     }
 
     // method to find whether a given string is in the TST
     public boolean find(String s) {
+        // Check edge cases
         if (s == null || s.length() == 0) {
             return false;
         }
 
+        // Identify if the string appears and return whether its a word
         NodeTST node = get(root, s, 0);
         return node != null && node.isWord;
     }
 
+    // Helper method to navigate through the TST to find a node
     public NodeTST get(NodeTST node, String s, int d) {
+        // Base case 1 is if no path exists
         if (node == null) {
             return null;
         }
 
+        // Base case 2 is if you searched past the end of the string
         if (d >= s.length()) {
             return null;
         }
 
         char c = s.charAt(d);
 
+        // Navigate using BST logic for current character
+        // If character is smaller go left, otherwise go right
         if (c < node.getCurrent()) {
-            return get(node.getLeft(), s, 0);
+            return get(node.getLeft(), s, d);
         } else if (c > node.getCurrent()) {
-            return get(node.getRight(), s, 0);
+            return get(node.getRight(), s, d);
+            // If the matching character is found then go middle
         } else if (d < s.length() - 1) {
             return get(node.getMiddle(), s, d + 1);
         } else {
+            // Return the last character
             return node;
         }
     }
